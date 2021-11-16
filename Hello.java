d3801828855b86d62e194f3ed0700ee9d8561276
Yasemin GONEN <81368784+yasemingonen@users.noreply.github.com>
2021-11-16 14:44:07 +0300

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello Can Bey, yaptik :)");
+        System.out.println("Hello Can Bey, yaptik :) :) :) :) v2");
     }
 }
