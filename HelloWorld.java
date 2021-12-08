974ca346a3241f52f8d35db1c8b4af611830b0d4
Yasemin GONEN <81368784+yasemingonen@users.noreply.github.com>
2021-12-08 10:44:22 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
-public class Hello {
 
+public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello World!");
+        System.out.println(" - java.version: " + System.getProperty("java.version"));
+        System.out.println(" - java.vm.version: " + System.getProperty("java.vm.version"));
     }
 }
